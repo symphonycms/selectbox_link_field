7f772354ea7316104e4cfa71112f450e61db98bd
Brendan Abbott <brendan@bloodbone.ws>
2010-07-09 11:01:17 +1000

    Encoding Bug..

--- fields/field.selectbox_link.php
+++ fields/field.selectbox_link.php
@@ -236,14 +236,13 @@
 				$primary_field = $this->__findPrimaryFieldValueFromRelationID($relation_id);
 
 				$value = $primary_field['value'];
-				if ($encode) $value = General::sanitize($value);
 
 				$item = new XMLElement('item');
 				$item->setAttribute('id', $relation_id);
 				$item->setAttribute('handle', Lang::createHandle($primary_field['value']));
 				$item->setAttribute('section-handle', $primary_field['section_handle']);
 				$item->setAttribute('section-name', General::sanitize($primary_field['section_name']));
-				$item->setValue($value);
+				$item->setValue(General::sanitize($value));
 
 				$list->appendChild($item);
 			}
