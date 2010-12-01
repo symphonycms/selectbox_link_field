8e7fc95811cce2cdcf218415352a1b533b148ef2
designermonkey <john.porter@designermonkey.co.uk>
2010-12-01 10:40:06 +0000

    Added value attribute to grouping of records

--- fields/field.selectbox_link.php
+++ fields/field.selectbox_link.php
@@ -77,7 +77,8 @@
 					$groups[$this->get('element_name')][$value] = array(
 						'attr' => array(
 							'link-id' => $data['relation_id'],
-							'link-handle' => Lang::createHandle($primary_field['value'])),
+							'link-handle' => Lang::createHandle($primary_field['value']),
+							'value' => $primary_field['value']),
 						'records' => array(),
 						'groups' => array()
 					);
