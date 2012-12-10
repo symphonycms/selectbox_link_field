b8fcab4bf9ffa6a99cb0c16a5f329be3891edcc8
alexbirukov <lord_maloy@mail.ru>
2012-12-11 00:54:49 +0600

    Update Russian language
    Update Russian language file

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -7,31 +7,33 @@
 			'email' => 'info@alexbirukov.ru',
 			'website' => 'http://alexbirukov.ru'
 		),
-		'release-date' => '2012-12-10'
+		'release-date' => '2012-12-11'
 	);
-	
-	
-	/*
-	 * EXTENSION: Field: Select Box Link
-	 * Localisation strings
-	 */
 
+	/**
+	 * Select Box Link Field
+	 */
 	$dictionary = array(
-	
+
+		'Allow selection of multiple options' => 
+		'Разрешить выбор нескольких опций.',
+
 		'Select Box Link' => 
 		'Ссылка на запись',
-		
-		'None' =>
+
+		'Limit to the %s most recent entries' => 
+		'Ограничение на %s последних записей.',
+
+		'None' => 
 		'Нет',
 
 		'Options' => 
 		'Опции',
 
-		'Limit to the %s most recent entries' => 
-		'Ограничение на %s последних записей.',
+		'Values' => 
+		'Значения',
 
-		'Allow selection of multiple options' => 
-		'Разрешить выбор нескольких опций.'
+		'Limit to %s entries' => 
+		'Лимит записей %s',
 
 	);
-	
